7445b671073bca5f0f2653e6ea0e5c7ab394deb1
Vetraxzone <72223535+Vetraxzone@users.noreply.github.com>
2026-07-07 09:16:11 +0200

    Device ID fail safe

--- customer.js
+++ customer.js
@@ -161,7 +161,21 @@ function getDeviceId() {
     let deviceId = localStorage.getItem("queue_device_id");
 
     if(!deviceId) {
-        deviceId = crypto.randomUUID();
+        if(crypto.randomUUID){
+            deviceId = crypto.randomUUID();
+        } else {
+            deviceId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+                .replace(/[xy]/g, function (c) {
+
+                    const r = Math.random() * 16 | 0;
+                    const v = c === 'x'
+                        ? r
+                        : (r & 0x3 | 0x8);
+
+                    return v.toString(16);
+                })
+        }
+        
         localStorage.setItem("queue_device_id", deviceId);
     }
 
